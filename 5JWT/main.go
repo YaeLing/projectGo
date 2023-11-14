112897f41279a0f345aa3d00f9f270d5ef1c0847
Harvey.Tung <harvey.tung@quantatw.com>
2023-11-14 16:25:37 +0800

    Add middleware authentication

--- 5JWT/main.go
+++ 5JWT/main.go
@@ -12,14 +12,25 @@ import (
 // 存在server的秘密字串，用來最後產生signature用的
 var jwtSecret = []byte("secret")
 
+type MyClaims struct {
+	UserID string `json:"userID"`
+	jwt.StandardClaims
+}
+
 // 當使用者成功登入後，應該是要生成一個jwt給他作為之後驗證使用
 func generateToken(userID string) string {
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
-		"userID": userID,
-		"exp":    time.Now().Add(time.Hour * 72).Unix(),
-	})
+	claims := MyClaims{
+		userID,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+			Issuer:    "Flynn",
+		},
+	}
+	//這邊注意signingMethod是SigningMethodHS256 不是 SigningMethodES256
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	tokenString, _ := token.SignedString(jwtSecret)
+	tokenString, err := token.SignedString(jwtSecret)
+	fmt.Println(tokenString, err)
 	return tokenString
 }
 
@@ -43,17 +54,28 @@ func authenticate(c *gin.Context) {
 }
 func main() {
 	server := setupRoute()
-	server.Run(":8888")
+	server.Run(":8080")
 }
 
 func setupRoute() *gin.Engine {
 	router := gin.Default()
-
-	router.GET("/hello", func(c *gin.Context) {
-		fmt.Printf("ctx.Request.Header: %v\n", c.Request.Header)
-	}, func(c *gin.Context) {
-		c.JSON(200, gin.H{"hello": "world"})
+	router.GET("/getToken/:userID", func(c *gin.Context) {
+		userID := c.Param("userID")
+		fmt.Println(userID)
+		fmt.Println(generateToken(userID))
+		c.JSON(200, gin.H{
+			"token": generateToken(userID),
+		})
 	})
-
+	//用Group去分類哪些要使用到middleware去做認證
+	//參考 https://gin-gonic.com/zh-tw/docs/examples/using-middleware/
+	authorize := router.Group("/authenticate")
+	authorize.Use(authenticate)
+	//這個括弧應該是可有可無 但就是把同個Group放在一起比較好看
+	{
+		authorize.GET("/", func(ctx *gin.Context) {
+			ctx.String(200, "OK")
+		})
+	}
 	return router
 }
